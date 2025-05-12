70afe0c9a56f6672d348e866d7afbe0dcc4ceecd
Hasan-TechNinja <hasantechninaj@gmail.com>
2025-05-13 05:34:11 +0600

    list

---
+++ list.py
@@ -0,0 +1,33 @@
+# student = ['Hasan', 'Habiba', 'Abir', 'Hakim', 'Ripon']
+
+# print(student)
+# for i in student:
+#     print(i)
+
+
+# if "Hasan" in student:
+#     print("Yes")
+# else:
+#     print('Noo')
+
+
+# student.append("new Name")
+# print(student)
+
+
+# student.insert(0, 'First value')
+# print(student)
+
+
+# print(student)
+# student.pop()
+# print(student)
+
+
+# item = student.clear()
+# print(item)
+
+
+# item = student.reverse()
+# print(item)
+
---
+++ sort.py
@@ -0,0 +1,4 @@
+num = [5, 3, 6, 9, 1, 0]
+print(num)
+num.sort()
+print(num)
