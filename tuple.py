1a0482f7cd7908271bba1b1e61ff095661641678
Hasan-TechNinja <hasantechninaj@gmail.com>
2025-05-13 14:27:35 +0600

    tuple

---
+++ tuple.py
@@ -0,0 +1,24 @@
+name = ('Hasan', 'Rakib', 'Hakim')
+
+# print(name)
+
+# print(type(name))
+
+# for i in name:
+#     print(i)
+
+# if "Mim" in name:
+#     print("Success!")
+# else:
+#     print("Error 4O4 not found!")
+
+
+# print(len(name))
+# print(name.count("Hakim"))
+
+
+# print(type(name))
+# ls = list(name)
+# print(type(ls))
+# print(name)
+# print(ls)
